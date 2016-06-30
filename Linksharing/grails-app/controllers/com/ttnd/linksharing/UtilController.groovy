8328f520176b78f84ca8771ca0c65404fab57e35
Jalaj Tagra <jalaj.tagra@tothenew.com>
2016-06-30 12:32:43 +0530

    Introduction to grails excercise

---
+++ Linksharing/grails-app/controllers/com/ttnd/linksharing/UtilController.groovy
@@ -0,0 +1,9 @@
+package com.ttnd.linksharing
+
+class UtilController {
+
+    def index() {
+        log.info("--------INFO--------------")
+        log.error(">>>>>>>>>>ERROR<<<<<<<<<<<<")
+    }
+}
